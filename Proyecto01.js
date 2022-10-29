7892f287807ab91ca78a734a210c7c345241587f
GutAn <eliu.anaya.gutierrez@outlook.com>
2022-10-29 16:13:52 -0500

    Se agrega el proyecto 02

---
+++ Proyecto01.js
@@ -0,0 +1,21 @@
+let free = false;
+const validarCliente=(time)=>{
+    let edad = prompt("¿Cual es tu edad?");
+    if(edad >= 18){
+        if(time >= 2 &&  time < 7 && free == false){
+            alert("Puedes pasar gratis, por que eres la primera persona despues de las 2 am");
+            free = true;
+        }else{
+            alert(`Son las ${time}:00 Hs ,puedes pasar pero debes pagar la entrada`);
+        }
+
+    }else{
+        alert("Eres menor de edad no puedes pasar");
+    }
+
+}
+
+validarCliente(3);
+validarCliente(4);
+validarCliente(7);
+
